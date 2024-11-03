3397da6ddf4ac7b58bce8003b399aa3a5ff47629
JORDI\Jordi <jordi_martinezjimenez@iescarlesvallbona.cat>
2024-11-03 17:41:23 +0100

    Middlewares for deleting notes when deleting task and deleting notes and tasks when deleting project

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,4 +1,5 @@
 import mongoose, { Schema, Document, Types } from "mongoose"
+import Note from "./Note"
 
 const taskStatus = {
     PENDING: "pending",
@@ -68,6 +69,13 @@ const TaskSchema: Schema = new Schema({
         }
     ]
 }, { timestamps: true })
+
+// Middleware
+TaskSchema.pre("deleteOne", { document: true, query: false }, async function () {
+    const taskId = this._id
+    if (!taskId) return
+    await Note.deleteMany({ task: taskId })
+})
 
 const Task = mongoose.model<ITask>("Task", TaskSchema)
 export default Task
